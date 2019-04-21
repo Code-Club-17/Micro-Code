4b2de096322658529dc395cf72f474a68dd9a83f
Said Gourida <33969482+Said113@users.noreply.github.com>
2019-04-21 20:44:06 +0200

    Update task1.py

--- Micro Code V2/solutions/python/task1.py
+++ Micro Code V2/solutions/python/task1.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 #this for task1
-#here in text you can input any letter(abd...,ABC....) or number(012....) or symbol(@&"().....)
+#here in the text "input" you can input any letter(abd...,ABC....) or number(012....) or symbol(@&"().....)
 
 
 import string as str
 
-letters=str.ascii_lowercase*2+str.ascii_uppercase*2+str.digits+str.punctuation+" " 
-_let=letters[3:-43]+letters[0:3]+letters[-43:]
-text=input()
+letters=str.ascii_lowercase*2+str.ascii_uppercase*2+str.digits+str.punctuation+" "  
+_let=letters[3:-43]+letters[0:3]+letters[-43:] # here slicing letters or drop 3 letters  
+text=input("please enter your text") # here the text you want to crypted
 res=""
 
 for _index,_char in enumerate(text):
-  items=text.replace(_char,_let[letters.index(_char)])
+  items=text.replace(_char,_let[letters.index(_char)])  # the result is "a"="a","A"="A"... etc
   res=res+items
 print(res[::len(text)+1])
