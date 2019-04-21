d093696a4bd19272eea39d5de58f8a888b36959f
Said Gourida <33969482+Said113@users.noreply.github.com>
2019-04-21 20:53:56 +0200

    Update task2.py

--- Micro Code V2/solutions/python/task2.py
+++ Micro Code V2/solutions/python/task2.py
@@ -7,12 +7,12 @@
 import string as str
 
 letters=str.ascii_lowercase*2+str.ascii_uppercase*2+str.digits+str.punctuation+" " 
-num=int(input())
+num=int(input("please enter your number between 0 - 25)) 
 _let=letters[num:-43]+letters[0:num]+letters[-43:]
-text=input()
+text=input("please enter your text") # feel free to input any words you want
 res=""
 
 for _index,_char in enumerate(text):
-  items=text.replace(_char,_let[letters.index(_char)])
+  items=text.replace(_char,_let[letters.index(_char)]) #letters here is case sensitive
   res=res+items
 print(res[::len(text)+1])
